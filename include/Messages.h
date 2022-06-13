3f8c14c011c2abba106bca589ae2d542a6c1fb37
avivek1997 <avivek1998@gmail.com>
2022-06-13 23:42:07 +0530

    Temperature Notification & Soil,Humidity Thing Speak Push

--- include/Messages.h
+++ include/Messages.h
@@ -5,4 +5,5 @@ struct Messages
     const char* BabyCry = "Baby is Crying";
     const char* FanOnAuto = "Fan Turned ON Due to High Temperature";   
     const char* FanOffAuto = "Fan Turned OFF Due to LOW Temperature";
+    const char* TemperatureAlert = "Alert!! Room Temperature is HIGH";
 }Messages;
